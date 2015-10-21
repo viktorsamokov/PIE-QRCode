55820936f83b125baf23015583bdabd1fbd1d8aa
Viktor Veljanoski <viktor_veljanoski@hotmail.com>
2015-10-22 01:16:24 +0200

    refactor app

--- src/pie_qrcode/qr_code/MainActivity.java
+++ src/pie_qrcode/qr_code/MainActivity.java
@@ -17,35 +17,39 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.TextView;
 
+/**
+ * @author Viktor
+ * Requires permission for Internet access - android.permission.INTERNET
+ **/   
 public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		setContentView(R.layout.activity_main);
-		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-	       intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-	       intent.putExtra("SAVE_HISTORY", false);//this stops saving ur barcode in barcode scanner app's history
-	       startActivityForResult(intent, 0);
-	       WebView view = (WebView) findViewById(R.id.webView);
-	       view.getSettings().setJavaScriptEnabled(true);
-		   view.setWebChromeClient(new WebChromeClient());
-	       
-	       
+		
+		Scan();
+	    
+	    // Enable Javascript execution in our webview
+	    WebView view = (WebView) findViewById(R.id.webView);
+	    view.getSettings().setJavaScriptEnabled(true);
+		view.setWebChromeClient(new WebChromeClient());   
 	}
 	
 	
 	@Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        // If the scanner scanned qr code get the result which in our case is url
         if (requestCode == 0) {
             if (resultCode == RESULT_OK) {
-                    String contents = data.getStringExtra("SCAN_RESULT"); //this is the result
+            		// This is the url from the scan
+                    String contents = data.getStringExtra("SCAN_RESULT"); 
                     finishActivity(0);
+                    
+                    // Load the contents from the url in our webview
                     WebView view = (WebView) findViewById(R.id.webView);
                     view.loadUrl(contents);
-                    
             } else 
             if (resultCode == RESULT_CANCELED) {
               // Handle cancel
@@ -56,40 +60,39 @@ public class MainActivity extends ActionBarActivity {
 	public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);                
         int landscape = newConfig.ORIENTATION_LANDSCAPE;
-        int portraite = newConfig.ORIENTATION_PORTRAIT;
         
+        // Reload the webview if the device screen is rotated
         if(newConfig.orientation == landscape){
         	WebView view = (WebView) findViewById(R.id.webView);
- 	       view.getSettings().setJavaScriptEnabled(true);
- 		   view.setWebChromeClient(new WebChromeClient());
+ 	        view.getSettings().setJavaScriptEnabled(true);
+ 		    view.setWebChromeClient(new WebChromeClient());
         }
-
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	/**
+	 * Handles onClick event when the user needs to do another scan
+	 * @param view
+	 */
+	public void onClickScan(View view)
+	{
+		Scan();
 	}
 	
-	public void Scan(View view){
-		   Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-	       intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-	       intent.putExtra("SAVE_HISTORY", false);
-	       startActivityForResult(intent, 0);
+	/**
+	 * Opens the scanner application and waits for the result from the scan
+	 */
+	public void Scan(){
+		// Prepare the intent for the qrcode scanner
+		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+				
+		//To scan QR code use "QR_CODE_MODE"
+	    intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+			    
+	    // This stops saving uour barcode in barcode scanner app's history
+	    intent.putExtra("SAVE_HISTORY", false);
+			    
+	    // Open the scanner app
+	    startActivityForResult(intent, 0);
 	}
 	
 }
